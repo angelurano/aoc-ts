9465532fbf376fb67950a71013fc30c80d00a105
Miguel Angel Garcia Beltran <angelurano00@gmail.com>
2025-12-02 13:40:58 +0100

    Solved ex 1 from day 02 (2025)

---
+++ src/2025/day02-1.ts
@@ -0,0 +1,28 @@
+// Path: src/2025/day02
+import { openInput } from '../openInput.ts';
+
+const file = openInput(2025, 2);
+
+const pairs = file.trim().split(',');
+const res = sumInvalidIDs(pairs);
+console.log(res);
+
+function sumInvalidIDs(pairs: string[]): number[]
+{
+  let summatory = 0;
+
+  pairs.forEach((pair) => {
+    const [left, right] = pair.split('-').map((num) => parseInt(num, 10));
+    for (let i = left; i <= right; i++)
+    {
+      const str = i.toString();
+      if (str.length % 2 != 0) continue;
+      const mid = str.length / 2;
+      let j = 0;
+      while (j < mid && str[j] === str[mid + j])
+        j++;
+      if (j === mid) summatory += i;
+    }
+  });
+  return summatory;
+}
